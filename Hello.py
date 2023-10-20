ee47aca4b623d6f59297733749eead2b9c601f1d
Ahmad Sohrabi <123218209+ASohraB@users.noreply.github.com>
2023-10-20 14:08:30 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -17,7 +17,28 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-
+class Celsci:
+    celscilist = {
+    'dd':{'x':{'followers': 400}},
+    'jp':{'x':{'followers': 300}},
+    'sh':{'x':{'followers': 200}},
+    'gs':{'x':{'followers': 100}}
+    }
+    asname="aso"
+    def fame(x):
+        y="low"
+        if x>300:
+            y= 'high'
+        return y
+ 
+def checkfame(x):
+    person = Celsci.fame(Celsci.celscilist[x]['x']['followers'])
+    return person
+    
+for person in Celsci.celscilist:
+    #dh=ic(person,checkfame(person))
+    print(person,checkfame(person))
+    
 def run():
     st.set_page_config(
         page_title="Hello",

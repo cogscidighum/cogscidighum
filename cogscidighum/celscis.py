fe1c0ff304c43a1800c68170ffc8a3f4ad972ecb
Ahmad Sohrabi <123218209+ASohraB@users.noreply.github.com>
2023-10-20 16:10:21 -0400

    Update celscis.py

--- cogscidighum/celscis.py
+++ cogscidighum/celscis.py
@@ -1,18 +1,18 @@
-class Celsci:
-    celscilist = {
+class Celsci(self):
+    self.celscilist = {
     'dd':{'x':{'followers': 900}},
     'jp':{'x':{'followers': 300}},
     'sh':{'x':{'followers': 200}},
     'gs':{'x':{'followers': 100}}
     }
     asname="aso"
-    def fame(x):
+    def fame(self,x):
         y="low"
         if x>300:
             y= 'high'
         return y
         
-     def checkfame(x1):
+     def checkfame(self,x1):
         person = fame(celscilist[x1]['x']['followers'])
         return person
 

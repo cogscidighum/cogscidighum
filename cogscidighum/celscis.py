56d925d45d1979c5791eb4610590f1edfbf2d728
Ahmad Sohrabi <123218209+ASohraB@users.noreply.github.com>
2023-10-20 17:04:05 -0400

    Update celscis.py

--- cogscidighum/celscis.py
+++ cogscidighum/celscis.py
@@ -15,10 +15,10 @@ class Celsci(self):
      def checkfame(self,x1):
         person = fame(celscilist[x1]['x']['followers'])
         return person
- def checkfame1(x2):
-    person = x2 + "-aso"
-    return person
+ #def checkfame1(x2):
+ #   person = x2 + "-aso"
+ #   return person
     
-for person in Celsci.celscilist:
-    #dh=ic(person,Celsci.checkfame(person))
-    print(person,Celsci.checkfame(person))
+#for person in Celsci.celscilist:
+#    #dh=ic(person,Celsci.checkfame(person))
+#    print(person,Celsci.checkfame(person))

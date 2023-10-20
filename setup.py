f66d85eaaaabb025e0ed5ac40fbb6127b28b906e
Ahmad Sohrabi <123218209+ASohraB@users.noreply.github.com>
2023-10-20 14:18:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+import celsci
+
+setup(
+    name='cogscidighum',
+    packages=['cogscidighum'],
+    version="1.0",
+    author='Carleton Cognitive Modelling Lab',
+    description='cogscidighum',
+    url='https://github.com/ASohraB/cogscidighum',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ]
+    )
